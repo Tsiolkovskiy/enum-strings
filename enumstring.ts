c87f79381ba7d6dd03b530bfd67f38b76be7316e
Tsiolkovskiy <56129993+Tsiolkovskiy@users.noreply.github.com>
2024-05-08 23:42:48 -0400

    Create enumstring.ts

---
+++ enumstring.ts
@@ -0,0 +1,9 @@
+enum Pet {Hamster = "HAMSTER", Rat = "RAT", Chinchilla = 'CHINCHILLA',  Tarantula = "TARANTULA" }
+const petOnSaleTS: Pet = Pet.Chinchilla;
+let ordersArrayTS:[Pet, number] [] = [
+  [Pet.Rat, 2], 
+  [Pet.Chinchilla, 1], 
+  [Pet.Hamster, 2], 
+  [Pet.Chinchilla, 50]
+];
+ordersArrayTS.push(['HAMSTER', 1]);
